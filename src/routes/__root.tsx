1d8199b32c7f9d2d72dae3e89783036deac9fce3
Myles Stupp <mylesstupp@gmail.com>
2026-08-03 00:59:53 -0400

    feat: add site favicon to root route links

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -21,6 +21,7 @@ export const Route = createRootRoute({
       }),
     ],
     links: [
+      { rel: 'icon', type: 'image/png', href: '/images/order_emblem.png' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous' },
       {
