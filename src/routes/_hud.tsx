80aaecc14d3f6ce034228d47ed9ab4068aa0fdc2
Myles Stupp <mylesstupp@gmail.com>
2026-08-21 09:57:06 -0400

    refactor: remove HudWorkspaceGhost from pendingComponent in _hud route

--- src/routes/_hud.tsx
+++ src/routes/_hud.tsx
@@ -1,9 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { HudLayout } from '@/components/hud/HudLayout'
-import { HudWorkspaceGhost } from '@/components/hud/HudGhostSkeletons'
 
 export const Route = createFileRoute('/_hud')({
   component: HudLayout,
-  pendingComponent: HudWorkspaceGhost,
 })
 
